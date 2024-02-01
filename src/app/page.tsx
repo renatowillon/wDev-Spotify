6caac38b003d1adf05d3a1f4af39170b4d9dff6e
“renatowillon” <renatowillon@hotmail.com>
2024-02-01 17:52:24 -0300

    ADD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen">
+    <div className="h-screen flex flex-col">
         
+        <div className="flex flex-1 p-2 gap-3">
+          <div className="w-64 gap-3">
+            <div className="bg-zinc-900 rounded-md">Menu</div>
+            <aside>Aside</aside>
+          </div>
+          <main className="flex-1 bg-zinc-900 rounded-md">Conteudo</main>
+        </div>
         
-        <div>Menu</div>
-        <aside>Aside</aside>
-        <main>Conteudo</main>
         <footer>Footer</footer>
-
 
     </div>
   );
