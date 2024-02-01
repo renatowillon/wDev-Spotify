510dc6f6d2aa5d3c6df405dc2e1908200acd9860
“renatowillon” <renatowillon@hotmail.com>
2024-02-01 17:59:52 -0300

    DESIGN PAGE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ export default function Home() {
     <div className="h-screen flex flex-col">
         
         <div className="flex flex-1 p-2 gap-3">
-          <div className="w-64 gap-3">
-            <div className="bg-zinc-900 rounded-md">Menu</div>
-            <aside>Aside</aside>
+          <div className="w-96 gap-3">
+            <div className="bg-zinc-900 rounded-md mb-3 h-28">Menu</div>
+            <aside className="bg-zinc-900 rounded-md mb-3">Aside</aside>
           </div>
           <main className="flex-1 bg-zinc-900 rounded-md">Conteudo</main>
         </div>
         
-        <footer>Footer</footer>
+        <footer className="bg-zinc-950 p-6">Footer</footer>
 
     </div>
   );
