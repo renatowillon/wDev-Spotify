0a38143f88aeb94b573082f1d6c7ccee249ae5aa
“renatowillon” <renatowillon@hotmail.com>
2024-02-08 18:08:57 -0300

    list

---
+++ src/components/ListPrimaria.tsx
@@ -0,0 +1,16 @@
+import React  from "react";
+
+interface MaisUsadasProps {
+    Image: string;
+    Cantor: string;
+}
+
+const MaisUsadas: React.FC<MaisUsadasProps> = (MaisUsadasProps) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default MaisUsadas;
